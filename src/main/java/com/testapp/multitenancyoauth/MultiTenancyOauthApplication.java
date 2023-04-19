07c2ba8e1ab7f29a057145d80689a34eaba35e4a
Michael <mreid@univeris.com>
2023-04-19 09:44:39 -0400

    multi-tenacay cahnages

---
+++ src/main/java/com/testapp/multitenancyoauth/MultiTenancyOauthApplication.java
@@ -0,0 +1,13 @@
+package com.testapp.multitenancyoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenancyOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiTenancyOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testapp/multitenancyoauth/MultiTenancyOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testapp.multitenancyoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenancyOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
